798cdd87ff7bd2716bde3e31d58f7076f2a3b3c9
Pavel <pavel.galkin@dxc.com>
2021-11-09 17:50:55 +0600

    ++

--- src/main/resources/templates/main.js
+++ src/main/resources/templates/main.js
@@ -1,28 +1,4 @@
-$.getJSON('/wagons', function(data){
-    // let table = document.getElementById("info").getElementsByTagName("TBODY")[0];
-    // $.each(data, function(key, val){
-    //     addRow();
-    //     let k = 1;
-    //     $.each(val, function (key1, val1){
-    //         table.getElementsByTagName("tr")[key+1].getElementsByTagName("td")[k].innerHTML = val1;
-    //         table.getElementsByTagName("tr")[key+1]
-    //             .getElementsByTagName("td")[k]
-    //             .getElementsByTagName("input")[0].value = val1;
-    //         k++;
-    //     });
-    //
-    // });
-    $('#info').append(`
-                  <tbody>${data.map(n => `
-                    <tr>
-                      <td>${n.id}</td>
-                      <td>${n.number}</td>
-                      <td>${n.weight}</td>
-                      <td>${n.type}</td>
-                    </tr>`).join('')}
-                  </tbody>
-`           );
-});
+
 
 // function addRow(){
 //     let tbody = document.getElementById("info").getElementsByTagName("TBODY")[0];
