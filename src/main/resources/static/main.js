cfdaa822fe08b6ef414c4ca1f61378e51194be34
Pavel <pavel.galkin@dxc.com>
2021-11-11 12:07:56 +0600

    update data

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $.getJSON('/wagons', function (data) {
+    let wagonId=0;
+
+    $.getJSON('/wagon', function (data) {
         $('#info').append(`
                   <tbody>${data.map(n => `
                     <tr>
@@ -12,39 +14,51 @@ $(document).ready(function () {
 `);
 
         $("tbody tr").click(function () {
-            let i = $(this).children()[0].textContent;
-            getWagon(i);
+            wagonId = $(this).children()[0].textContent;
+            getWagon(wagonId);
         });
-
-        $("#q").click(function () {
-
-        });
-
     });
 
-
-    function getWagon(id1) {
-        $.getJSON(`/wagon/${id1}`,
-            function (data) {
-                $("#number").val(data.number);
-                $("#weight").val(data.weight);
-                $("#type").val(data.type);
-            });
-    }
-
+    $("#q").click(function () {
+        if(wagonId===0){
+            alert("No row for update selected")
+        }else {
+            let data = {
+                "id": wagonId,
+                "number": $("#number").val(),
+                "weight": $("#weight").val(),
+                "type": $("#type").val()
+            }
+            updateWagon(data);
+        }
+    });
 
 });
 
-// function addRow(){
-//     let tbody = document.getElementById("info").getElementsByTagName("TBODY")[0];
-//     let row = document.createElement("TR")
-//     for (let i=0;i<4;i++){
-//         let td = document.createElement("TD")
-//         // td.appendChild(document.createTextNode("column " + i))
-//         row.appendChild(td);
-//         let input = document.createElement("input");
-//         input.setAttribute('type', 'text');
-//         td.appendChild(input);
-//     }
-//     tbody.appendChild(row);
-// }
+function getWagon(id) {
+    $.getJSON(`/wagon/${id}`,
+        function (data) {
+            $("#number").val(data.number);
+            $("#weight").val(data.weight);
+            $("#type").val(data.type);
+        });
+}
+
+function updateWagon(data){
+    $.ajax({
+        type: 'PUT',
+        url: '/wagon',
+        contentType: 'application/json',
+        data: JSON.stringify(data), // access in body
+    }).done(function () {
+        window.location.reload(true);
+        console.log('SUCCESS');
+    }).fail(function (msg) {
+        console.log('FAIL');
+    }).always(function (msg) {
+        console.log('ALWAYS');
+    });
+}
+
+
+
