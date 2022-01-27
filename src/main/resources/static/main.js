6e64c7225176655766c9184e29516caae8980381
Pavel <pavel.galkin@dxc.com>
2022-01-27 17:58:58 +0600

    разделил на две страницы

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,9 +1,7 @@
 let wagonId = 0;
-let resourceType = "";
 $(document).ready(()=>{
 
     getAllWagons();
-    $('#kafka').hide();
     $("#q").click(()=> {
         if (wagonId === 0) {
             alert("No row for update selected");
@@ -15,12 +13,6 @@ $(document).ready(()=>{
             weight: $("#weight").val(),
             type: $("#type").val()
         });
-    });
-
-    //Обработчики:
-
-    $('input[name="int_ext"]').click( ()=> {
-        readResources($("input[type='radio']:checked").val());
     });
 
     //выбираем все теги с именем  modal
@@ -53,22 +45,6 @@ $(document).ready(()=>{
     $("#clone").click(()=> {
         cloneWagon();
     });
-
-    $('#resourceData').click(()=>{
-       getNSIResourceData();
-    });
-
-    //-------wagon lifecycle-----
-    $("#far").click(sendFarArrival);
-
-    $("#inner").click(sendInnerInvoice);
-
-    $("#inv").click(sendInvoice);
-
-    $("#cur").click(sendCurrentLocation);
-
-    $("#weigh").click(sendWeighing);
-
 
 });
 //Функции:
@@ -128,75 +104,6 @@ function createWagon(){
         }), // access in body
     }).done(function () {
         console.log(`data sent to backend`);
-    });
-}
-
-function readResources(type){
-    resourceType = type;
-    $('#resources').find('option').remove();
-    $.getJSON(`/resource/${type}`, (items) =>{
-        $.each(items, (key, value)=> {
-            $('#resources')
-                .append($("<option></option>")
-                    .attr("value", key)
-                    .text(value));
-        });
-    });
-}
-
-function getNSIResourceData(){
-    let opt = $("#resources option:selected").text();
-    $('.resource').find('p').remove();
-    $.getJSON(`/resourceData/${opt}/${resourceType}`,
-        function (data) {
-            $.each(data, function (key, value){
-                $('.resource').append("<p>" + key+ " : " + value + "</p>")
-            });
-        });
-    $('#kafka').fadeIn(1000);
-    if(resourceType==='internal'){
-        $("#far,#inv").hide();
-        $("#inner").show();
-    }else {
-        $("#inner").hide();
-        $("#far,#inv").show();
-    }
-}
-
-function sendFarArrival(){
-    let inv = $('#invoice').val();
-    $.post(`/farArrival/${inv}`, function (){
-        console.log(`Far arrival data sent to backend`);
-        });
-}
-
-function sendInvoice(){
-    let inv = $('#invoice').val();
-    $.post(`/invoice/${inv}`, function (){
-        console.log(`Invoice data sent to backend`);
-    });
-}
-
-function sendCurrentLocation(){
-    let path = $('#path').val();
-    let time = $('#time').val();
-    $.post(`/currentLocation/${path}&${time}`, function () {
-        console.log(`Current location data sent to backend`);
-    });
-}
-
-function sendWeighing(){
-    let brutto = $('#brutto').val();
-    let tare = $('#tare').val();
-    $.post(`/weght?brutto=${brutto}&tare=${tare}&isExternal=${resourceType}`,
-        ()=>{console.log(`weighing data sent to backend`);
-    });
-}
-
-function sendInnerInvoice(){
-    let inv = $('#invoice').val();
-    $.post(`/innerInvoice/${inv}`, function (){
-        console.log(`Invoice data sent to backend`);
     });
 }
 
